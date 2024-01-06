7bcb447df1e1fff67f38ebc54842ef98645725a6
jo-duchan <jo_duchan@icloud.com>
2024-01-07 00:16:14 +0900

    Chapter 47. CreateTodo widget

--- lib/pages/todos_page.dart
+++ lib/pages/todos_page.dart
@@ -23,6 +23,7 @@ class _TodosPageState extends State<TodosPage> {
             child: Column(
               children: [
                 TodoHeader(),
+                CreateTodo(),
               ],
             ),
           ),
@@ -52,6 +53,37 @@ class TodoHeader extends StatelessWidget {
           ),
         )
       ],
+    );
+  }
+}
+
+class CreateTodo extends StatefulWidget {
+  const CreateTodo({super.key});
+
+  @override
+  State<CreateTodo> createState() => _CreateTodoState();
+}
+
+class _CreateTodoState extends State<CreateTodo> {
+  final newTodoController = TextEditingController();
+
+  @override
+  void dispose() {
+    newTodoController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      controller: newTodoController,
+      decoration: InputDecoration(labelText: 'What to do?'),
+      onSubmitted: (String? todoDesc) {
+        if (todoDesc != null && todoDesc.trim().isNotEmpty) {
+          context.read<TodoList>().addTodo(todoDesc);
+          newTodoController.clear();
+        }
+      },
     );
   }
 }
