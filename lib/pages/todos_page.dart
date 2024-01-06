03efcf5c87f7244040eed24b9787e6b86c949802
jo-duchan <jo_duchan@icloud.com>
2024-01-07 00:06:02 +0900

    Chapter 46. TodoHeader widget

--- lib/pages/todos_page.dart
+++ lib/pages/todos_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:todo_provider/providers/providers.dart';
 
 class TodosPage extends StatefulWidget {
   const TodosPage({super.key});
@@ -10,10 +12,46 @@ class TodosPage extends StatefulWidget {
 class _TodosPageState extends State<TodosPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text('TODOS'),
+    return SafeArea(
+      child: Scaffold(
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 20,
+              vertical: 40,
+            ),
+            child: Column(
+              children: [
+                TodoHeader(),
+              ],
+            ),
+          ),
+        ),
       ),
+    );
+  }
+}
+
+class TodoHeader extends StatelessWidget {
+  const TodoHeader({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text(
+          'TODO',
+          style: TextStyle(fontSize: 40),
+        ),
+        Text(
+          '${context.watch<ActiveTodoCount>().state.activeTodoCount} items left',
+          style: TextStyle(
+            fontSize: 20,
+            color: Colors.redAccent,
+          ),
+        )
+      ],
     );
   }
 }
